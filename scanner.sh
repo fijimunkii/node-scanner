b172b4c80dcc7654ab4831e1616df440b41a153e
Harrison Powers <harrisonpowers@gmail.com>
2017-07-31 17:08:52 -0400

    Better log directory name

--- scanner.sh
+++ scanner.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOGDIR="$DIR/node-scanner-logs"
+
 # Check for primitives being overwritten
 PRIMITIVES=( String Number Object Array Boolean Symbol )
 for TYPE in "${PRIMITIVES[@]}"
@@ -8,11 +11,9 @@ do
 done
 
 # Run Scanners on each top level directory
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 ls -d */ | xargs -I {} {
-  bash -c "cd {} && notes > $DIR/logs/notes.log"
-  bash -c "cd {} && nsp check --output summary > $DIR/logs/nsp.log"
-  bash -c "cd {} && retire > $DIR/logs/retire.log"
-  bash -c "cd {} && plato -r -d report -l .jshintrc -t 'My Awesome App' -x 'node_modules|bower_components' . > $DIR/logs/plato.log"
-} > $DIR/logs/scan.log
+  bash -c "cd {} && notes > $LOGDIR/notes.log"
+  bash -c "cd {} && nsp check --output summary > $LOGDIR/nsp.log"
+  bash -c "cd {} && retire > $LOGDIR/retire.log"
+  bash -c "cd {} && plato -r -d report -l .jshintrc -t 'My Awesome App' -x 'node_modules|bower_components' . > $LOGDIR/plato.log"
+} > $LOGDIR/scan.log
