17a13432d30eafa3885b9ca53702c2ac5b9ccea0
Harrison Powers <harrisonpowers@gmail.com>
2017-07-31 16:43:41 -0400

    Add scanners

--- scanner.sh
+++ scanner.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# Check for primitives being overwritten
 PRIMITIVES=( String Number Object Array Boolean Symbol )
 for TYPE in "${PRIMITIVES[@]}"
 do
   grep -r "\b$TYPE\.prototype\.[A-z0-9 ]*=" . > logs/primitives.log
 done
 
+# Run Scanners on each top level directory
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ls -d */ | xargs -I {} bash -c "cd {} && nsp check --output summary > $DIR/logs/nsp.log"
+
+ls -d */ | xargs -I {} {
+  bash -c "cd {} && nsp check --output summary > $DIR/logs/nsp.log"
+  bash -c "cd {} && retire > $DIR/logs/retire.log"
+  bash -c "cd {} && plato -r -d report -l .jshintrc -t 'My Awesome App' -x 'node_modules|bower_components' . > $DIR/logs/plato.log"
+} > $DIR/logs/scan.log
