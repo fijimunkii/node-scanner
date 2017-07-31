73ee3523df0f40222622f67083f40d7752a773c2
Harrison Powers <harrisonpowers@gmail.com>
2017-07-31 16:50:29 -0400

    Add notes

--- scanner.sh
+++ scanner.sh
@@ -11,6 +11,7 @@ done
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ls -d */ | xargs -I {} {
+  bash -c "cd {} && notes > $DIR/logs/notes.log"
   bash -c "cd {} && nsp check --output summary > $DIR/logs/nsp.log"
   bash -c "cd {} && retire > $DIR/logs/retire.log"
   bash -c "cd {} && plato -r -d report -l .jshintrc -t 'My Awesome App' -x 'node_modules|bower_components' . > $DIR/logs/plato.log"
